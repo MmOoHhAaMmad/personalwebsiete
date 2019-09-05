b315cd33beac0413475c14e251f754f5c59bc0dc
MmOoHhAaMmad <54144803+MmOoHhAaMmad@users.noreply.github.com>
2019-09-05 16:40:03 +0200

    addwebsiete

---
+++ js/js.js
@@ -0,0 +1,17 @@
+//window.onload(alert("Hallo"))
+function imgDesc (){
+    
+ p=document.getElementById("firstP")
+    if (p.style.display === "none") {
+        p.style.display="block"
+         } else {
+           p.style.display="none"      }
+}
+function imgDesc1 (){
+    
+    p=document.getElementById("firstP1")
+    if (p.style.display === "none") {
+       p.style.display="block"
+          } else {
+            p.style.display="none"      }
+}
